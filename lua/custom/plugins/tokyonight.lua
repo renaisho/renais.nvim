32ead55149ee0b2ea5575ba7fc5b5571eef49aec
Ho <renais@foundi.info>
2025-01-17 12:37:19 +0800

    Change the colorscheme to tokyonight

--- lua/custom/plugins/tokyonight.lua
+++ lua/custom/plugins/tokyonight.lua
@@ -1,11 +1,9 @@
 return {
   {
-    'shaunsingh/nord.nvim',
+    'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
-      vim.g.nord_italic = false
-      vim.g.nord_disable_background = true
-      vim.cmd.colorscheme 'nord'
+      vim.cmd.colorscheme 'tokyonight-night'
 
       -- You can configure highlights by doing something like:
       -- vim.cmd.hi 'Comment gui=none'
