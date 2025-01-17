3df368bc6ba9820bb3b8a8d2cdfdfd409f2c30bb
Ho <renais@foundi.info>
2025-01-17 12:40:38 +0800

    Disable the highlight of treesitter

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -9,7 +9,7 @@ return {
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
-        enable = true,
+        enable = false,
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
